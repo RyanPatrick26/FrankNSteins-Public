3dca9b94ecfca1c80a883e74f2b7ee6b6a5cbadb
Ryan Patrick <ryan.patrick26@stclairconnect.ca>
2016-11-28 10:43:56 -0500

    Built the initial layout for the menu and menu items

---
+++ app/src/main/java/com/example/install/franknsteins/MenuItem.java
@@ -0,0 +1,15 @@
+package com.example.install.franknsteins;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Ryan Patrick on 11/28/2016.
+ */
+public class MenuItem {
+    private TextView itemName;
+    private TextView itemDescription;
+    private TextView itemPrice;
+    private TextView addButton;
+    private ImageView itemImage;
+}
