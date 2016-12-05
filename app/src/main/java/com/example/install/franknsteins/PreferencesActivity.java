d58101fe26816d09640dab10d1e4d1f7ce77d94d
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2016-12-05 00:01:33 -0500

    #22: Attempting with PreferenceActivity

---
+++ app/src/main/java/com/example/install/franknsteins/PreferencesActivity.java
@@ -0,0 +1,13 @@
+package com.example.install.franknsteins;
+
+import android.preference.PreferenceActivity;
+import android.os.Bundle;
+
+public class PreferencesActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
