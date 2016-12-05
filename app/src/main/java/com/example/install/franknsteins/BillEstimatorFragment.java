275cbf586593e6292f050d7a208c7ae192547d30
Nicholas Allaire <nicholas.allaire85@stclairconnect.ca>
2016-12-05 17:52:44 -0500

    #22: Cleaning up unnecessary code.

--- app/src/main/java/com/example/install/franknsteins/BillEstimatorFragment.java
+++ app/src/main/java/com/example/install/franknsteins/BillEstimatorFragment.java
@@ -1,23 +1,14 @@
 package com.example.install.franknsteins;
 
-
-
-import android.support.v4.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
-import java.util.Locale;
 
 
 /**
